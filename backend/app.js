781a81484eb3276e2cf4d4d0da63700350b2461a
SlothKai <khfkai@hotmail.com>
2023-09-30 01:44:03 +0800

    Added Firebase Services

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ var app = express();
 //app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -25,17 +27,6 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../config');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+//get all user
+router.get ("/", async function(req, res, next) {
+    try {
+        const snapshot = await User.get();
+        const users = [];
+        snapshot.forEach((doc) => {
+            const data = doc.data();
+            users.push(data);
+        });
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+//Add user..?
+router.post("/add", async function(req, res, next) {
+    try {
+      const data = req.body;
+      console.log("Data of Users:", data);
+      await User.add(data);
+      res.send({ msg: "Added User." });
+    } catch (error) {
+      console.error("Error creating document:", error);
+      res.status(500).send({ error: "Failed to create document" });
+    }
+  });
+
+router.post("/update", async function(req, res, next) {
+    try {
+        const id = req.body.id;
+        delete req.body.id;
+        const data = req.body;
+        await User.doc(id).update(data);
+        res.send({ msg: "Updated User Details." });
+    } catch (error) {
+        console.error("Error updating document:", error);
+        res.status(500).send({ error: "Failed to update document" });
+    }
+});
+
+router.post("/delete", async function(req, res, next) {
+    try {
+        const id = req.body.id;
+        await User.doc(id).delete();
+        res.send({ msg: "Deleted User." });
+    } catch (error) {
+        console.error("Error deleting document:", error);
+        res.status(500).send({ error: "Failed to delete document" });
+    }
 });
 
 module.exports = router;

1688c15423d27c0bdca612654e786974181ee66b
Owen Lee <owen0004@e.ntu.edu.sg>
2023-10-01 20:29:54 +0800

    Added ability to edit user profile and refresh lis

--- backend/routes/openai.js
+++ backend/routes/openai.js
@@ -91,11 +91,15 @@ router.post("/", async function (req, res, next) {
       const leavesLeft = employee_details.leavesLeft - daydiff;
 
       if (leavesLeft < 0) {
-        return JSON.stringify({ msg: `Not enough leaves. You need ${daydiff} but only have ${employee_details.leavesLeft}` });
+        return JSON.stringify({
+          msg: `Not enough leaves. You need ${daydiff} but only have ${employee_details.leavesLeft}`,
+        });
       } else {
         employee_details.leavesLeft = leavesLeft;
         await usersFunctions.update_details(employee_details);
-        return JSON.stringify({ msg: `Leave application is successful. ${employee_details.name} has ${employee_details.leavesLeft} leaves left.}`});
+        return JSON.stringify({
+          msg: `Leave application is successful. ${employee_details.name} has ${employee_details.leavesLeft} leaves left.}`,
+        });
       }
     } catch (error) {
       console.log("Error: " + error);
@@ -106,16 +110,16 @@ router.post("/", async function (req, res, next) {
   const delete_employee = async (employee) => {
     try {
       const status = await usersFunctions.delete_employee(employee);
-      if(status.success == "true"){
+      if (status.success == "true") {
         return JSON.stringify({ msg: `Employee ${employee} has been deleted` });
-      } else{
+      } else {
         return JSON.stringify({ msg: `Employee ${employee} not found` });
       }
     } catch (error) {
       console.log("Error: " + error);
       return JSON.stringify({ error: "Error deleting employee" });
     }
-  }
+  };
 
   //Declare functions for GPT
   const functions = [
@@ -200,7 +204,7 @@ router.post("/", async function (req, res, next) {
   const askGPT = async (messageToSend) => {
     let response = await openai.chat.completions.create({
       //gpt-3.5-turbo-0613
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: messageToSend,
       functions: functions,
       function_call: "auto",
@@ -221,7 +225,6 @@ router.post("/", async function (req, res, next) {
     }
 
     try {
-
       let response = await askGPT(messageToSend);
       let executeFunctions = {};
 
@@ -237,7 +240,7 @@ router.post("/", async function (req, res, next) {
         }
 
         const function_name = message.function_call.name;
-        console.log("function name: ", function_name)
+        console.log("function name: ", function_name);
 
         if (executeFunctions[function_name]) {
           console.log("EXECUTED BEFORE!!: ", function_name);
@@ -263,9 +266,7 @@ router.post("/", async function (req, res, next) {
             break;
           case "delete_employee":
             const functionArgs3 = JSON.parse(message.function_call.arguments);
-            function_response = await delete_employee(
-              functionArgs3.employee
-            );
+            function_response = await delete_employee(functionArgs3.employee);
             break;
         }
 
--- hr-gen2/src/containers/Home/index.tsx
+++ hr-gen2/src/containers/Home/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { columns } from "@/utils/egdata";
 import {
   Button,
   Card,
@@ -27,14 +28,14 @@ import {
   User,
   useDisclosure,
 } from "@nextui-org/react";
-import toast from "react-hot-toast";
-import { columns } from "@/utils/egdata";
+import axios from "axios";
+import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import * as yup from "yup";
 import { Pencil } from "../../../public/icons/pencil.jsx";
 import { Trash } from "../../../public/icons/trash.jsx";
-import axios from "axios";
-import { Field, Form, Formik, FormikHelpers, useFormik } from "formik";
-import * as yup from "yup";
+import { Reload } from "../../../public/icons/reload.jsx";
 
 require("dotenv").config();
 
@@ -84,6 +85,47 @@ const addEmployee = async (
   }
 };
 
+const updateEmployee = async (
+  employeeInfo: EditEmployeeValues,
+  fetchEmployees: () => void
+) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}users/update`,
+      {
+        id: employeeInfo.id,
+        name: employeeInfo.name,
+        email: employeeInfo.email,
+        department: employeeInfo.department,
+        role: employeeInfo.role,
+        skillset: employeeInfo.skillset,
+        leavesLeft: employeeInfo.leavesLeft,
+        leavesTotal: employeeInfo.leavesTotal,
+        avatar: employeeInfo.avatar,
+      }
+    );
+
+    if (res.status === 200) {
+      toast.success("Employee updated!");
+      fetchEmployees();
+    }
+  } catch (e) {
+    toast.error("An error has occured!");
+    console.log(e);
+  }
+};
+interface UserType {
+  id: string;
+  name: string;
+  email: string;
+  avatar: string;
+  department: string;
+  role: string;
+  skillset: string;
+  leavesLeft: string;
+  leavesTotal: string;
+}
+
 interface AddEmployeeValues {
   name: string;
   email: string;
@@ -93,6 +135,19 @@ interface AddEmployeeValues {
   leavesTotal: string;
   avatar: string;
 }
+
+interface EditEmployeeValues {
+  id: string;
+  name: string;
+  email: string;
+  department: string;
+  role: string;
+  skillset: string;
+  leavesLeft: string;
+  leavesTotal: string;
+  avatar: string;
+}
+
 interface AddEmployeeModalProps {
   isOpen?: boolean;
   onOpenChange?: (open: boolean) => void;
@@ -270,11 +325,19 @@ const AddEmployeeModal: React.FC<AddEmployeeModalProps> = ({
                 <Input
                   label="Avatar Link"
                   variant="bordered"
-                  name="avatarLink"
-                  id="avatarLink"
+                  name="avatar"
+                  id="avatar"
                   value={addEmployeesFormik.values.avatar}
                   onChange={addEmployeesFormik.handleChange}
                   onBlur={addEmployeesFormik.handleBlur}
+                  isInvalid={
+                    addEmployeesFormik.touched.avatar &&
+                    Boolean(addEmployeesFormik.errors.avatar)
+                  }
+                  errorMessage={
+                    addEmployeesFormik.touched.avatar &&
+                    addEmployeesFormik.errors.avatar
+                  }
                 />
               </ModalBody>
               <ModalFooter>
@@ -299,26 +362,271 @@ const AddEmployeeModal: React.FC<AddEmployeeModalProps> = ({
   );
 };
 
+interface EditEmployeeModalProps {
+  isOpen?: boolean;
+  employeeId: string;
+  employeeInfo: UserType[];
+  onOpenChange?: (open: boolean) => void;
+  fetchEmployees?: () => void;
+}
+
+const EditEmployeeModal: React.FC<EditEmployeeModalProps> = ({
+  isOpen,
+  employeeId,
+  employeeInfo,
+  onOpenChange,
+  fetchEmployees,
+}) => {
+  // get employee id from list of employees
+  let employee = employeeInfo.find((emp) => emp.id === employeeId);
+
+  const validationSchema = yup.object({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("Enter a valid email")
+      .required("Email is required"),
+    department: yup.string().required("Department is required"),
+    role: yup.string().required("Role is required"),
+    skillset: yup.string().required("Skillsets are required"),
+    leavesLeft: yup.string().required("Leaves remaining is required"),
+    leavesTotal: yup.string().required("Total leaves are required"),
+  });
+
+  const editEmployeesFormik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      department: "",
+      role: "",
+      skillset: "",
+      leavesLeft: "",
+      leavesTotal: "",
+      avatar: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      updateEmployee({ ...values, id: employeeId }, () => fetchEmployees?.());
+      onClose();
+    },
+  });
+
+  React.useEffect(() => {
+    if (!employee) return;
+    editEmployeesFormik.setValues({
+      name: employee.name,
+      email: employee.email,
+      department: employee.department,
+      role: employee.role,
+      skillset: employee.skillset,
+      leavesLeft: employee.leavesLeft,
+      leavesTotal: employee.leavesTotal,
+      avatar: employee?.avatar,
+    });
+  }, [employeeId, onOpenChange]);
+
+  const onClose = () => {
+    editEmployeesFormik.resetForm();
+    onOpenChange?.(false);
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      onOpenChange={onOpenChange}
+      placement="top-center"
+      backdrop="blur"
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              Edit employee: {employee!.name}
+            </ModalHeader>
+            <form
+              onSubmit={editEmployeesFormik.handleSubmit}
+              id="add-employee-form"
+            >
+              <ModalBody>
+                <Input
+                  autoFocus
+                  label="Name"
+                  variant="bordered"
+                  name="name"
+                  id="name"
+                  value={editEmployeesFormik.values.name}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.name &&
+                    Boolean(editEmployeesFormik.errors.name)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.name &&
+                    editEmployeesFormik.errors.name
+                  }
+                  isRequired
+                ></Input>
+                <Input
+                  label="Email"
+                  variant="bordered"
+                  name="email"
+                  type="email"
+                  id="email"
+                  value={editEmployeesFormik.values.email}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.email &&
+                    Boolean(editEmployeesFormik.errors.email)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.email &&
+                    editEmployeesFormik.errors.email
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Department"
+                  variant="bordered"
+                  name="department"
+                  id="department"
+                  value={editEmployeesFormik.values.department}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.department &&
+                    Boolean(editEmployeesFormik.errors.department)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.department &&
+                    editEmployeesFormik.errors.department
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Role"
+                  variant="bordered"
+                  name="role"
+                  id="role"
+                  value={editEmployeesFormik.values.role}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.role &&
+                    Boolean(editEmployeesFormik.errors.role)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.role &&
+                    editEmployeesFormik.errors.role
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Skillsets"
+                  variant="bordered"
+                  name="skillset"
+                  id="skillset"
+                  value={editEmployeesFormik.values.skillset}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.skillset &&
+                    Boolean(editEmployeesFormik.errors.skillset)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.skillset &&
+                    editEmployeesFormik.errors.skillset
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Leaves remaining"
+                  variant="bordered"
+                  name="leavesLeft"
+                  id="leavesLeft"
+                  value={editEmployeesFormik.values.leavesLeft}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.leavesLeft &&
+                    Boolean(editEmployeesFormik.errors.leavesLeft)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.leavesLeft &&
+                    editEmployeesFormik.errors.leavesLeft
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Total leaves"
+                  variant="bordered"
+                  name="leavesTotal"
+                  id="leavesTotal"
+                  value={editEmployeesFormik.values.leavesTotal}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.leavesTotal &&
+                    Boolean(editEmployeesFormik.errors.leavesTotal)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.leavesTotal &&
+                    editEmployeesFormik.errors.leavesTotal
+                  }
+                  isRequired
+                />
+                <Input
+                  label="Avatar Link"
+                  variant="bordered"
+                  name="avatar"
+                  id="avatar"
+                  value={editEmployeesFormik.values.avatar}
+                  onChange={editEmployeesFormik.handleChange}
+                  onBlur={editEmployeesFormik.handleBlur}
+                  isInvalid={
+                    editEmployeesFormik.touched.avatar &&
+                    Boolean(editEmployeesFormik.errors.avatar)
+                  }
+                  errorMessage={
+                    editEmployeesFormik.touched.avatar &&
+                    editEmployeesFormik.errors.avatar
+                  }
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="danger"
+                  variant="flat"
+                  onPress={() => {
+                    onClose();
+                  }}
+                >
+                  Discard
+                </Button>
+                <Button color="success" variant="flat" type="submit">
+                  Save
+                </Button>
+              </ModalFooter>
+            </form>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+};
+
 const Home = () => {
   const [isPromptGenerating, setIsPromptGenerating] = useState<boolean>(false);
   const [inputPrompt, setInputPrompt] = useState<string>("");
   const [outputPrompt, setOutputPrompt] = useState<string>("");
   const [employees, setEmployees] = useState<UserType[]>([]);
   const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [employeeEditId, setEmployeeEditId] = useState<string>("");
 
   const addEmployeeModalController = useDisclosure();
-
-  interface UserType {
-    id: number;
-    name: string;
-    email: string;
-    avatar: string;
-    department: string;
-    role: string;
-    skillset: string;
-    leavesLeft: number;
-    leavesTotal: number;
-  }
+  const editEmployeeModalController = useDisclosure();
 
   const handleInputFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputPrompt(e.target.value);
@@ -338,7 +646,7 @@ const Home = () => {
   };
 
   const deleteEmployee = async (
-    employeeId: number,
+    employeeId: string,
     fetchEmployees: () => void
   ) => {
     setIsDeleting(true);
@@ -367,6 +675,24 @@ const Home = () => {
 
       if (data) {
         setEmployees(data.data);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleToggleEditEmployeeModal = (employeeId: string) => {
+    if (employeeId != null) {
+      setEmployeeEditId(employeeId);
+      editEmployeeModalController.onOpen();
+    }
+  };
+
+  const handleListReload = async () => {
+    try {
+      const data = await getEmployees();
+      if (data) {
+        toast.success("Employee list refreshed!");
       }
     } catch (e) {
       console.log(e);
@@ -428,13 +754,17 @@ const Home = () => {
           return (
             <div className="relative flex items-center gap-2">
               <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <span
+                  className="text-lg text-default-400 cursor-pointer active:opacity-50"
+                  onClick={() => {
+                    handleToggleEditEmployeeModal(employees.id);
+                  }}
+                >
                   <Pencil />
                 </span>
               </Tooltip>
               <Popover placement="bottom-end">
                 <PopoverTrigger>
-                  {/* <Button>Open Popover</Button> */}
                   <span className="text-lg text-danger cursor-pointer active:opacity-50">
                     <Trash />
                   </span>
@@ -486,15 +816,25 @@ const Home = () => {
       content: (
         <>
           <div className="flex flex-col space-y-5">
-            <div className="mr-auto">
-              <Button
-                color="secondary"
-                variant="flat"
-                className="w-fit"
-                onClick={addEmployeeModalController.onOpen}
-              >
-                Add Employee
-              </Button>
+            <div className="flex items-center">
+              <div className="mr-auto">
+                <Button
+                  color="secondary"
+                  variant="flat"
+                  className="w-fit"
+                  onClick={addEmployeeModalController.onOpen}
+                >
+                  Add Employee
+                </Button>
+              </div>
+              <div>
+                <div
+                  className="bg-gray-200 p-2 rounded-md hover:bg-gray-300 group hover:cursor-pointer"
+                  onClick={handleListReload}
+                >
+                  <Reload className="stroke-gray-500 group-hover:stroke-gray-900" />
+                </div>
+              </div>
             </div>
             <Table
               aria-label="Example table with custom cells"
@@ -527,6 +867,14 @@ const Home = () => {
             isOpen={addEmployeeModalController.isOpen}
             onOpenChange={addEmployeeModalController.onOpenChange}
             fetchEmployees={fetchEmployees}
+          />
+
+          <EditEmployeeModal
+            isOpen={editEmployeeModalController.isOpen}
+            onOpenChange={editEmployeeModalController.onOpenChange}
+            fetchEmployees={fetchEmployees}
+            employeeId={employeeEditId}
+            employeeInfo={employees}
           />
         </>
       ),
